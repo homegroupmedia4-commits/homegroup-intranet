a24afc8fd3919da605ae5218859c5f8d47b39e6f
homegroupmedia4-commits <homegroupmedia4@gmail.com>
2026-03-27 09:14:00 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const uploadRoutes = require("./routes/upload");
+const groupRoutes = require("./routes/group.routes");
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/upload", uploadRoutes);
-
+app.use("/api/group", groupRoutes);
 /* ======================
    🔌 DB
 ====================== */
