c488ff77248a81df66becf8af0671c92ac14c9df
homegroupmedia4-commits <homegroupmedia4@gmail.com>
2026-03-30 10:19:17 +0200

    Update news.routes.js

--- server/routes/news.routes.js
+++ server/routes/news.routes.js
@@ -16,4 +16,13 @@ router.get("/", getNews);
 // DELETE
 router.delete("/:id", deleteNews);
 
+router.put("/:id/pin", async (req, res) => {
+  const news = await require("../models/News").findById(req.params.id);
+
+  news.pinned = !news.pinned;
+  await news.save();
+
+  res.json(news);
+});
+
 module.exports = router;
