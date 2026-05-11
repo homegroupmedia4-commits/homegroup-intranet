d4f56b3afb8d091f406a43fbd53078f2b20f8d57
homegroupmedia4-commits <homegroupmedia4@gmail.com>
2026-05-11 14:30:19 +0200

    Update App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -7,6 +7,12 @@ import Organisation from "./pages/Organisation";
 import Contact from "./pages/Contact";
 import Admin from "./pages/admin/Admin";
 
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+
 function App() {
   return (
     <BrowserRouter>
@@ -16,7 +22,18 @@ function App() {
           <Route path="/groupe" element={<Groupe />} />
           <Route path="/organisation" element={<Organisation />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/admin" element={<Admin />} />
+        <Route
+  path="/admin"
+  element={
+    <ProtectedRoute>
+      <Admin />
+    </ProtectedRoute>
+  }
+/>
+
+          <Route path="/login" element={<Login />} />
+<Route path="/forgot-password" element={<ForgotPassword />} />
+<Route path="/reset-password" element={<ResetPassword />} />
         </Routes>
       </Layout>
     </BrowserRouter>
